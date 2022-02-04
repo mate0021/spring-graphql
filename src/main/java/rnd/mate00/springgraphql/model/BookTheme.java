acf5600b8160a8f493625ea3f720c2cbbddb1764
mate0021 <mami.ext@gmail.com>
2022-02-04 11:02:29 +0100

    Fixed theme name in query.

--- src/main/java/rnd/mate00/springgraphql/model/BookTheme.java
+++ src/main/java/rnd/mate00/springgraphql/model/BookTheme.java
@@ -2,8 +2,18 @@ package rnd.mate00.springgraphql.model;
 
 public enum BookTheme {
 
-    REPORT,
-    FINANCE,
-    SPORTS,
-    HORROR
+    REPORT("Report"),
+    FINANCE("Finance"),
+    SPORTS("Sports"),
+    HORROR("Horror");
+
+    private final String themeName;
+
+    BookTheme(String themeName) {
+        this.themeName = themeName;
+    }
+
+    public String getThemeName() {
+        return themeName;
+    }
 }
